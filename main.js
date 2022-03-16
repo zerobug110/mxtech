cd51e68c98565ccacc736dfa4dab9e45b7f3287c
zerobug110 <mahabojang110@gmail.com>
2022-03-16 13:00:00 +0000

    complete about section

---
+++ main.js
@@ -0,0 +1,60 @@
+//HAMBURGER NAV-BAR 
+
+//get the  nav items
+const burger = document.querySelector('.lines');
+const navLinks = document.querySelector('.nav-bar__items')
+const open = document.querySelector('.nav-bar__show');
+const links = document.querySelectorAll('.nav-bar__links')
+const date = document.getElementById('date');
+
+// toggle when click
+burger.addEventListener('click', () => {
+    navLinks.classList.toggle('nav-bar__show');
+});
+
+
+//////////////////////////modal///////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+//variables
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button');
+const overlay = document.getElementById('overlay');
+
+//open modal when click on project
+openModalButtons.forEach( button => {
+    button.addEventListener('click',() => {
+        const modal = document.querySelector(button.dataset.modalTarget);
+        openModal(modal);
+    });
+});
+
+//close modal when click outside of the modal
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active');
+    modals.forEach(modal => {
+        closeModal(modal)
+    });
+});
+
+//close modal when click on project
+closeModalButtons.forEach( button => {
+    button.addEventListener('click',() =>  {
+        const modal = button.closest('.modal');
+        closeModal(modal);
+    });
+});
+
+
+//add active class when on project modal
+function openModal(modal) {
+    if (modal == null) return 
+    modal.classList.add('active');
+    overlay.classList.add('active');
+}
+
+//remove class when click close
+function closeModal(modal) {
+    if (modal == null) return 
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
+}
